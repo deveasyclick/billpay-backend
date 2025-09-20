639745483f7aa585a1800f5af18bf2d2db6541cf
Eyohen <henry.eyo2@gmail.com>
2025-09-20 21:04:36 +0100

    Redesigned airtime and data page

--- apps/web/app/buy-airtime/layout.tsx
+++ apps/web/app/buy-airtime/layout.tsx
@@ -1,3 +1,13 @@
+//app/buy-airtime/layout.tsx
+// export default function BuyAirtimeLayout({
+//   children,
+// }: Readonly<{
+//   children: React.ReactNode;
+// }>) {
+//   return <section className="w-full">{children}</section>;
+// }
+
+
 export default function BuyAirtimeLayout({
   children,
 }: Readonly<{
