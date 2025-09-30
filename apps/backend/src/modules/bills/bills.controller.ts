e3930411608a77b84bfe5606bdf6455f8a08b72c
Yusuf <easyclick05@gmail.com>
2025-09-30 22:16:30 +0100

    feat: add api for validating customer

--- apps/backend/src/modules/bills/bills.controller.ts
+++ apps/backend/src/modules/bills/bills.controller.ts
@@ -9,6 +9,10 @@ import { ApiBody, ApiOkResponse, ApiOperation, ApiTags } from '@nestjs/swagger';
 import { BillsService } from './bills.service';
 import { PayBillDTO, PayBillResponseDTO } from './dtos/payment';
 import { GetBillerItemsResponseDto } from './dtos/item';
+import {
+  ValidateCustomerDTO,
+  ValidateCustomerResponseDTO,
+} from './dtos/validate-customer';
 
 @Controller('bills')
 @ApiTags('Bills')
@@ -52,5 +56,25 @@ export class BillsController {
       message: 'Success',
       data: await this.billsService.fetchAllPlans(),
     };
+  }
+
+  @Post('validate-customer')
+  @ApiOperation({ summary: 'Validate customer info' })
+  @ApiBody({ type: ValidateCustomerDTO })
+  @ApiOkResponse({ type: ValidateCustomerDTO })
+  async validateCustomer(
+    @Body() dto: ValidateCustomerDTO,
+  ): Promise<ValidateCustomerResponseDTO> {
+    try {
+      const res = await this.billsService.validateCustomer(dto);
+      return {
+        statusCode: 200,
+        message: 'Success',
+        data: res,
+      };
+    } catch (err) {
+      console.log('err', err?.response?.data ?? err);
+      throw new InternalServerErrorException('Payment failed');
+    }
   }
 }
